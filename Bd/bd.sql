891e86fb6e63400eb2bd672d25fc0125a6131162
Amandaalmeida12 <amandaisabel237@gmail.com>
2017-10-31 14:35:26 -0300

    ambiente criado

---
+++ Bd/bd.sql
@@ -0,0 +1,32 @@
+CREATE TABLE products(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	descricao VARCHAR(80) NOT NULL,
+	quantidade_atual INTEGER NOT NULL,
+	preco_por_caixa FLOAT(5) NOT NULL,
+	demanda INTEGER NOT NULL,
+	created DATETIME not null,
+	modified DATETIME not null,
+	constraint id_pk primary key(id)
+);
+
+
+CREATE TABLE users(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	name VARCHAR(40) NOT NULL,
+	register VARCHAR(12) NOT NULL,
+	password VARCHAR(70) NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	constraint cpf_pk primary key(id)
+);
+CREATE TABLE lots(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	quantidade_entrada INTEGER NOT NULL,
+	entrada DATETIME NOT NULL,
+	validade DATETIME NOT NULL,
+	quantidade_saida INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	id_funcionario INTEGER NOT NULL,
+	constraint pk_id primary key(id),
+	constraint product_id_fk foreign key(product_id) references products(id),
+	constraint id_fk foreign key(id_funcionario) references users(id)
+);
