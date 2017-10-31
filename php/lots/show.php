642a2bb561b63f6037885cbc7d7eb183511bedec
Amandaalmeida12 <amandaisabel237@gmail.com>
2017-10-31 16:00:34 -0300

    lots concluido

---
+++ php/lots/show.php
@@ -0,0 +1,14 @@
+<?php 
+	include '../../Bd/conexao.php';
+
+	$srt = $pdo->query("SELECT * FROM lots");
+ 
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>lots</title>
+ </head>
+ 
+ </body>
+ </html>
